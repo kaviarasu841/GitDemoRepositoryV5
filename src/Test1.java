7a909459ed5957e1f74d0e5a737791dd8e06699a
kaviarasu1234 <kaviarasu.s@dxc.com>
2024-11-08 11:29:34 +0530

    Test1

--- src/Test1.java
+++ src/Test1.java
@@ -1,11 +1,8 @@
 public class Test1 {	
 	public static void main(String args[]) {
 		String hungry = "yes";
-		if(hungry == "yes" || hungry == "Y" || hungry == "y")
+		if(hungry == "yes")
 			System.out.println("eat something....");
-		else if(hungry == "Y" || hungry == "y") {
-			System.out.println("eat something....");
-		}
 		else {
 			System.out.println("Do work...");
 		}
